bee877aa69d9a610e112682f5834e4a94d84f72d
JGUETO <joseeguerreroot@gmail.com>
2018-02-15 12:00:58 -0500

    For

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -4,27 +4,31 @@ import './Image.css'
 import $ from 'jquery'
 
 export default class Image extends React.Component{
+    constructor(props){
+        super(props);
+    }
     animacion(){
         var timerId = setInterval(function(){
             let actualMargin = parseInt($('.animacion').css('margin-top'));
             $('.animacion').css({'margin-top': actualMargin-240});
             if(actualMargin <= -2880){
-                console.log('asdasd')
                 $('.animacion').css({'margin-top': '0px'});
                 clearInterval(timerId);
+                return true;
             }
         },100);
     }
 
     componentDidMount(){
         this.animacion();
+        console.log('sdass')
     }
 
     render(){
-        return(
+        return(            
             <li>
                 <img className="animacion" src={this.props.showImage} />
-            </li>
+            </li> 
         )
     }
 }
--- src/components/ImageList/index.js
+++ src/components/ImageList/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import Image from '../Image'
 
 export default class ImageList extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            links : [
+                "http://hetah.net/_assets/modules/traductor/img/b/bienvenido.jpg",
+                "http://hetah.net/_assets/modules/traductor/img/u/universidad.jpg",
+                "http://hetah.net/_assets/modules/traductor/img/alfabeto/d.jpg",
+                "http://hetah.net/_assets/modules/traductor/img/alfabeto/e.jpg",
+                "http://hetah.net/_assets/modules/traductor/img/alfabeto/l.jpg",
+                "http://hetah.net/_assets/modules/traductor/img/m/magdalena.jpg"
+            ]   
+        }
+    }
+
     render(){
         return(
             <div className="listImages">
                 <ul className="ulImages">
-                    <Image showImage="http://www.hetah.net/_assets/modules/traductor/img/c/comida.jpg" />
+                    <Image showImage={this.state.links} />
                 </ul>
             </div>
         )
