118439e5f3e582b7368297a893a9691baa121819
Albeiro <albeiroespitiasierra@gmail.com>
2018-02-15 21:24:12 -0500

    Test de animacion con varias imagenes

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -4,26 +4,40 @@ import './Image.css'
 import $ from 'jquery'
 
 export default class Image extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            imageList : this.props.showImage,
+            actualImage : this.props.showImage[0]
+        }
+    }
+
     animacion(){
-        var timerId = setInterval(function(){
+        var animationTimes = 0;
+        var timerId = setInterval(()=>{
             let actualMargin = parseInt($('.animacion').css('margin-top'));
             $('.animacion').css({'margin-top': actualMargin-240});
-            if(actualMargin <= -2880){
-                console.log('asdasd')
+            if(actualMargin <= -2500){
                 $('.animacion').css({'margin-top': '0px'});
-                clearInterval(timerId);
+                animationTimes++;
+                this.setState({
+                    actualImage : this.state.imageList[animationTimes]
+                })
+                if(animationTimes >= this.state.imageList.length){
+                    clearInterval(timerId);
+                }
             }
         },100);
     }
 
     componentDidMount(){
-        this.animacion();
+        this.animacion()
     }
 
     render(){
         return(
             <li>
-                <img className="animacion" src={this.props.showImage} />
+                <img className="animacion" src={this.state.actualImage} />
             </li>
         )
     }
--- src/components/ImageList/index.js
+++ src/components/ImageList/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import Image from '../Image'
 
 export default class ImageList extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            links : [
+                "http://www.hetah.net/_assets/modules/traductor/img/c/comida.jpg",
+                "http://www.hetah.net/_assets/modules/traductor/img/c/casa.jpg",
+                "http://www.hetah.net/_assets/modules/traductor/img/c/carro.jpg",
+                "http://www.hetah.net/_assets/modules/traductor/img/a/ala.jpg",
+                "http://www.hetah.net/_assets/modules/traductor/img/b/burro.jpg",
+                "http://www.hetah.net/_assets/modules/traductor/img/p/persona.jpg",
+                "http://www.hetah.net/_assets/modules/traductor/img/i/iman.jpg"
+            ]
+        }
+    }
+
     render(){
         return(
             <div className="listImages">
                 <ul className="ulImages">
-                    <Image showImage="http://www.hetah.net/_assets/modules/traductor/img/c/comida.jpg" />
+                    <Image showImage={this.state.links} />
                 </ul>
             </div>
         )
