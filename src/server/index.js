39dbee1467f5e26e415a2f3d3e634f86c2bdfebb
Albeiro <albeiroespitiasierra@gmail.com>
2018-02-22 13:08:28 -0500

    Arreglando error con promesas

--- src/server/index.js
+++ src/server/index.js
@@ -23,21 +23,54 @@ app.post('/consulta',(req,res)=>{
     var data = req.body.frase;
     let consulta;
     let respuesta = [];
-    // Si en la oracion, existen 2 palabras que corresponden una seña
-    // Comparamos las palabras de 2 en 2
-    for(let i=1;i<data.length;i++){
-        consulta = `SELECT * FROM imagen WHERE nombre = "${data[i-1] +' '+ data[i]}"`;
-        connection.query(consulta, function (error, results, fields) {
-            if (error) throw error;
 
-            if(results.length > 0){ // Si existe una seña que tenga 2 palabras
-                data[i-1] = data[i-1] +" "+data[i]; // Unidos jamas seran vencidos
-                data.splice(i, 1);
+    query(data).then(result => {
+        console.log(data);
+        // implement your success case...
+
+    }).catch(err => {
+        //throw exception here...
+    });
+
+});
+
+function query(data){
+
+    return new Promise((resolve, reject) => {
+
+        var counter = 0;
+
+        if (data.length > 0){
+
+            for(let i=1;i<data.length;i++){
+
+                consulta = `SELECT * FROM imagen WHERE nombre = "${data[i-1] +' '+ data[i]}"`;
+
+                connection.query(consulta, function (error, results, fields) {
+                    if (error) reject(error); //terminate the promisse...
+
+                    if(results.length > 0){ // Si existe una seña que tenga 2 palabras
+                        data[i-1] = data[i-1] +" "+data[i]; // Unidos jamas seran vencidos
+                        data.splice(i, 1);
+                    }
+
+                    counter++;
+
+                    if (counter >= data.length){
+                        resolve("Everything OK");
+                    }
+
+                });
+
             }
-        });
-    }
 
-})
+        } else {
+            reject("There are no data"); //terminate the promise...
+        }
+
+    });
+
+}
 
 
 app.get('*',(req,res)=>{
